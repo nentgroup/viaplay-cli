7f9e3f72a035013bb8e30c5e02e4d4bf340c11da
Alessandro Coletti <alessandro.coletti@gmail.com>
2025-08-05 19:40:36 +0200

    fix: update banner printing
    viaplay-banner now only shows on the root and help commands.

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -31,7 +31,15 @@ team or organization standards such as rulesets, secrets, and environments.
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	printBanner()
+	// Set a pre-run hook for the root command to display the banner
+	// This will only show the banner for 'vip' or 'vip help' commands
+	rootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
+		// Only show the banner if it's the root command or help command
+		if cmd.Name() == rootCmd.Name() || cmd.Name() == "help" {
+			printBanner()
+		}
+	}
+
 	err := rootCmd.Execute()
 	if err != nil {
 		output.VerboseMessage("Root command execution failed")
